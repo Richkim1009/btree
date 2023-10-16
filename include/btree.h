241b654adddc09e6207482c11df842bf1ea32f5f
richkim1009 <gerrad1009@daum.net>
2023-10-16 06:53:38 +0000

    btree insert implement

--- include/btree.h
+++ include/btree.h
@@ -8,15 +8,19 @@ class BTree {
 public: 
     BTree(size_t degree): mDegree(degree), mRoot(std::make_unique<Node<T>>(degree)) {}
 
-    void Insert(const T&& key)
+    void Insert(const T key)
     {
+        // 트리가 빈 경우
         if (mRoot == nullptr) {
+            // 트리를 만들고 키 삽입
             mRoot = std::make_unique<Node<T>>(mDegree);
-            mRoot->getKeys.emplace_back(std::forward(key));
+            mRoot->GetKeys().emplace_back(std::move(key));
         } else {
-            mRoot->getKeys().emplace_back(std::forward(key));
+            mRoot->Insert(std::move(key));
         }
     }
+
+
 private:
     size_t mDegree;
     std::unique_ptr<Node<T>> mRoot;
--- include/node.h
+++ include/node.h
@@ -5,14 +5,71 @@
 template <typename T>
 class Node {
 public:
-    Node(sizt_t degree): mDegree(degree) {}
+    Node(size_t degree): mDegree(degree) {}
 
-    size_t getDegree() const {return mDegree;}
-    std::vector<T>& getKeys() {return mkeys;}
-    const std::vector<T>& getKeys() const {return mKeys;}
-    std::vector<std::unique_ptr<Node<T>>>& getChildren() {return mChildren;}
-    const std::vector<std::unique_ptr<Node<T>>>& getChildren() const {return mChildren;}
+    size_t GetDegree() const {return mDegree;}
+    std::vector<T>& GetKeys() {return mKeys;}
+    const std::vector<T>& GetKeys() const {return mKeys;}
+    std::vector<std::unique_ptr<Node<T>>>& GetChildren() {return mChildren;}
+    const std::vector<std::unique_ptr<Node<T>>>& GetChildren() const {return mChildren;}
 
+    std::unique_ptr<Node<T>> Insert(T key)
+    {
+        std::unique_ptr<Node<T>> retPtr = nullptr;
+        const size_t index = findIndex(key);
+        if (mChildren.empty()) {
+            if (mKeys.size() + 1 == mDegree) {
+                retPtr = split(key, index);
+            }
+            mKeys.insert(mKeys.begin()+index, key);
+        } else {
+            mChildren[index]->Insert(std::move(key));
+        }
+
+        return retPtr;
+    }
+private:
+    size_t findIndex(const T& key)
+    {
+        typename std::vector::const_iterator it = std::upper_bound(mKeys.begin(), mKeys.end(), key);
+        const size_t index = std::distance(it, mKeys.begin());
+        return index;
+    }
+
+    std::unique_ptr<Node<T>> split(T key, size_t index)
+    {
+        size_t mid = (mKeys.size() - 1)/2;
+        std::unique_ptr<Node<T>> parentNode = std::make_unique<Node<T>>{mDegree};
+        std::unique_ptr<Node<T>> rightChild = std::make_unique<Node<T>>{mDegree};
+        std::unique_ptr<Node<T>> leftChild = std::make_unique<Node<T>>{mDegree};
+        if (index == mid) {
+            parentNode->GetKeys().emplace_back(std:move(key));
+            leftChild->GetKeys().insert(leftChild->GetKeys().end(), 
+                std::move_iterator(mKeys.begin(), std::move_iterator(mKeys.begin()+mid)));
+            rightChild->GetKeys().insert(rightChild->Getkeys().end(),
+                std::move_iterator(mKeys.begin()+mid), std::move_iterator(mKeys.end()));
+        } else if (index < mid) {
+            parentNode->GetKeys().emplace_back(std::move(mKeys[mid]));
+            leftChild->GetKeys().insert(leftChild->GetKeys().end(),
+                std::move_iterator(mKeys.begin(), mKeys.begin()+index));
+            leftChild->GetKeys().insert(leftChild->GetKeys().end(),
+                std::move_iterator(mKeys.begin()+index, mKeys.begin()+mid));
+            rightChild->GetKey().insert(rightChild->GetKeys().end(),
+                std::move_iterator(mKeys.begin()+mid+1, mKeys.end()));
+        } else {
+            parentNode->GetKeys().emplace_back(std::move(mKeys[mid]));
+            leftChild->GetKeys().insert(leftChild->GetKeys().end(),
+                std::move_iterator(mKeys.begin(), mKeys.begin()+index));
+            rightChild->GetKey().insert(rightChild->GetKeys().end(),
+                std::move_iterator(mKeys.begin()+mid+1, mKeys.begin()+index));
+            rightChild->GetKeys().insert(rightChild->GetKeys().end(),
+                std::move_iterator(mKeys.begin()+index, mKeys.end()));
+        }
+        parentNode->GetChildren().emplace_bacK(std::move(leftChild));
+        parentNode->GetChildren().emplace_bacK(std::move(rightChild));
+
+        return parentNode;
+    }
 private:
     size_t mDegree;
     std::vector<Node<T>> mKeys;
