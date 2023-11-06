663c8f67bc287ce2241cac038c10bb563ebb6d30
polaris1009 <gerrad1009@daum.net>
2023-11-06 16:03:15 +0000

    delete

--- build/first_test[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "/home/polaris/study/cpp/btree/build/first_test[1]_tests.cmake")
-  include("/home/polaris/study/cpp/btree/build/first_test[1]_tests.cmake")
-else()
-  add_test(first_test_NOT_BUILT first_test_NOT_BUILT)
-endif()
